415918805f0e71157f7e1a0ef640e2a75821fc05
KajiPhucSE <hoangphucnguyen234@gmail.com>
2026-05-26 11:25:14 +0700

    Update StudentController.cs

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -6,16 +6,16 @@ namespace StudentManagement.Controllers
     {
         public IActionResult Info()
         {
-            // Truyền dữ liệu bằng ViewBag (Động)
+            
             ViewBag.Name = "Nguyễn Hoàng Phúc";
 
-            // Truyền dữ liệu bằng ViewData (Dạng Dictionary)
+            
             ViewData["Age"] = 20;
 
-            // Truyền dữ liệu bằng Model (Khuyên dùng vì an toàn và chặt chẽ nhất)
+            
             string major = "CNTT";
 
-            // Ép kiểu (object) để hệ thống hiểu đây là Model data, không phải tên của View
+            
             return View((object)major);
         }
     }
