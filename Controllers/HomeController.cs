d86aab2db2c8df9fe8f8bdeffc30cc3ee8d34e15
KajiPhucSE <hoangphucnguyen234@gmail.com>
2026-05-26 11:24:27 +0700

    Remove comments from HomeController methods

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,19 +4,19 @@ namespace StudentManagement.Controllers
 {
     public class HomeController : Controller
     {
-        // Hiển thị "Welcome to ASP.NET MVC"
+        
         public string Index()
         {
             return "Welcome to ASP.NET MVC";
         }
 
-        // Hiển thị tên sinh viên
+       
         public string About()
         {
             return "Tên sinh viên: Nguyễn Hoàng Phúc";
         }
 
-        // Hiển thị email sinh viên
+     
         public string Contact()
         {
             return "Email: hoangphucnguyen234@gmail.com";
